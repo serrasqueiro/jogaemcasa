8de3cb9cf5deb29778232460380912ffe39e74be
Henrique Moreira <h@serrasqueiro.com>
2023-09-23 14:19:21 +0100

    Update from https://www.slbenfica.pt/futebol/calendario

--- slb_dates.py
+++ slb_dates.py
@@ -1,60 +1,83 @@
 #-*- coding: ISO-8859-1 -*-
-#
-# SLB dates at home!
+
+""" SLB dates at home, and visiting! """
+
+# pylint: disable=missing-function-docstring
 
 import json
 import datetime
+from slbschedule import JOGOS_EM_CASA
 
-# source: https://www.slbenfica.pt/pt-pt/jogos/calendario
-# alt source: https://www.zerozero.pt/edition.php?id_edicao=165864
-JOGOS_EM_CASA = (
-    ["19-Aug-2023 20:30", "Estrela da Amadora"],
-    ["02-Sep-2023 20:30", "Guimaraes"],
-    ["01-Oct-2023", "FC Porto"],
-    ["29-Oct-2023", "Casa Pia AC"],
-    ["12-Nov-2023", "Sporting"],
-    ["10-Dec-2023", "SC Farense"],
-    ["29-Dec-2023", "FC Famalicao"],
-    ["14-Jan-2024", "Rio Ave"],
-    ["21-Jan-2024", "Boavista"],
-)
+LANG = "pt"
 
 DATES = JOGOS_EM_CASA
 
+VALID_WHAT = {
+    "League": "Liga Portuguesa",
+    "champions": "Champeons League",
+    "europa": "Liga Europa",
+    "Tac,a Liga": "Taça da Liga",
+}
+
 def main():
-    res = list()
+    res = []
+    last = None
     for tup in DATES:
-        what = "League"
-        if len(tup) == 3:
-            date, who, what = tup
-        else:
-            date, who = tup
-        full = False
-        try:
-            when = strptime(date, "%d-%b-%Y %H:%M")
-            full = True
-        except ValueError:
-            when = strptime(date, "%d-%b-%Y")
-        weekday = "?" if when is None else convert_to_pt_weekday(when.weekday())
-        if weekday != "?":
-            data = when.strftime("%a, %d %b %H:%M") if full else when.strftime("%a, %d %b (???)")
-        else:
-            data = "?"
-        #print(data, what, "SLB vs", who)
-        item = {
-            "date": date,
-            "weekday": weekday,
-            "house": "SLB (Liga)" if what.startswith("L") else "SLB",
-            "visitor": who,
-        }
+        item, _, when = item_from(tup)
         res.append(item)
+        if last is not None:
+            diff_days = difference_days(last, when)
+            assert diff_days > 1, "difference too small"
+        last = when
     json_string = json_dumper(res)
     #print(f"::: START\n{json_string}\n<<< END")
     dump_to(open("slb_dates.json", "w", encoding="ascii"), json_string)
 
+
+def item_from(tup):
+    what = "League"
+    where = "(casa)"
+    if len(tup) == 4:
+        date, who, temp_what, where = tup
+        if temp_what is not None:
+            what = temp_what
+    elif len(tup) == 3:
+        date, who, what = tup
+    else:
+        date, who = tup
+    full = False
+    try:
+        when = strptime(date, "%d-%b-%Y %H:%M")
+        full = True
+    except ValueError:
+        when = strptime(date, "%d-%b-%Y")
+    weekday = "?" if when is None else convert_to_weekday(when, LANG)
+    if weekday == "?":
+        data = "?"
+    else:
+        data = when.strftime("%a, %d %b %H:%M") if full else when.strftime("%a, %d %b (???)")
+    is_liga = what in ("League",)
+    if where == "(casa)":
+        house_str = "SLB" + (" (Liga)" if is_liga else "")
+        visitor_str = who
+        print(data, what + "!", "SLB vs", who, "; what:", what, "; where:", where)
+    else:
+        house_str = who
+        visitor_str = "SLB"
+        print(data, what + "!", house_str, "vs", visitor_str, "; what:", what, "; where:", where)
+    item = {
+        "date": date,
+        "weekday": weekday,
+        "house": house_str,
+        "visitor": visitor_str,
+        "what": what.title(),
+    }
+    assert what in VALID_WHAT, data + "! " + what
+    return item, data, when
+
 def json_dumper(alist:list) -> str:
     a_str = json.dumps(alist, indent=2, sort_keys=True)
-    return a_str
+    return a_str + "\n"
 
 def dump_to(outfile, astr):
     outfile.write(astr)
@@ -64,12 +87,20 @@ def strptime(*args):
         return None
     return datetime.datetime.strptime(*args)
 
-def convert_to_pt_weekday(wday:int, lang="pt") -> str:
+def convert_to_weekday(when=None, lang="pt") -> str:
+    if when is None:
+        when = datetime.datetime.now()
+    wday_str = when.strftime("%A")
     if not lang:
-        weekday = when.strftime("%A")
-        return weekday
+        return wday_str
     days = ("2a.feira", "3a.feira", "4a.feira", "5a.feira", "6a.feira", "sabado", "domingo",)
-    return days[wday]
+    wday_str = days[when.weekday()]
+    return wday_str
+
+def difference_days(t0, t1):
+    t_delta = t1 - t0
+    #print("t1 - t0:", t1, t0, "; is:", t_delta.days)
+    return t_delta.days
 
 if __name__ == "__main__":
     main()
--- slbschedule.py
+++ slbschedule.py
@@ -1,6 +1,6 @@
 #-*- coding: ISO-8859-1 -*-
-#
-# slbschedule.py
+
+""" slbschedule.py - Sport Lisboa e Benfica, calendario """
 
 # source: https://www.slbenfica.pt/pt-pt/jogos/calendario
 # alt source: https://www.zerozero.pt/edition.php?id_edicao=165864
@@ -9,10 +9,20 @@ JOGOS_EM_CASA = (
     ["02-Sep-2023 20:30", "Guimaraes"],
     ["29-Sep-2023 20:15", "FC Porto"],
     ["03-Oct-2023 20:00", "Inter Milao", "champions", "Milan"],
-    ["29-Oct-2023", "Casa Pia AC"],
-    ["12-Nov-2023", "Sporting"],
-    ["10-Dec-2023", "SC Farense"],
-    ["29-Dec-2023", "FC Famalicao"],
+    ["07-Oct-2023 20:30", "Estoril Praia", None, "Estoril"],
+    ["24-Oct-2023 20:00", "Real Sociedad", "champions"],
+    ["28-Oct-2023 18:00", "Casa Pia AC"],
+    ["31-Oct-2023 20:15", "Arouca", "Tac,a Liga"],
+    ["04-Nov-2023 15:30", "Chaves",],
+    ["08-Nov-2023 17:45", "Real Sociedad", "champions", "San Sebastian"],
+    ["12-Nov-2023 20:30", "Sporting"],
+    ["29-Nov-2023 20:00", "Inter Milao", "champions"],
+    ["03-Dec-2023 18:00", "Moreirense"],
+    ["08-Dec-2023 18:00", "SC Farense"],
+    ["12-Dec-2023 20:00", "Salzburgo", "champions", "Salzburgo"],
+    ["21-Dec-2023 20:15", "AVS Futebol SAD", "Tac,a Liga"],
+    ["29-Dec-2023 18:45", "FC Famalicao"],
+    ["07-Jan-2024", "Arouca", None, "Arouca"],
     ["14-Jan-2024", "Rio Ave"],
     ["21-Jan-2024", "Boavista"],
 )
