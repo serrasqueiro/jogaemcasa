0560d0b86ed7412599d3b7400787cbd0f30f28e9
Henrique Moreira <h@serrasqueiro.com>
2023-09-23 13:38:02 +0100

    Horarios do SLB quando houve o sorteio da Champeons

---
+++ slbschedule.py
@@ -0,0 +1,18 @@
+#-*- coding: ISO-8859-1 -*-
+#
+# slbschedule.py
+
+# source: https://www.slbenfica.pt/pt-pt/jogos/calendario
+# alt source: https://www.zerozero.pt/edition.php?id_edicao=165864
+JOGOS_EM_CASA = (
+    ["19-Aug-2023 20:30", "Estrela da Amadora"],
+    ["02-Sep-2023 20:30", "Guimaraes"],
+    ["29-Sep-2023 20:15", "FC Porto"],
+    ["03-Oct-2023 20:00", "Inter Milao", "champions", "Milan"],
+    ["29-Oct-2023", "Casa Pia AC"],
+    ["12-Nov-2023", "Sporting"],
+    ["10-Dec-2023", "SC Farense"],
+    ["29-Dec-2023", "FC Famalicao"],
+    ["14-Jan-2024", "Rio Ave"],
+    ["21-Jan-2024", "Boavista"],
+)
